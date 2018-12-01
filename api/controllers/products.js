6c5bca241e119a2809c46bc9d4f7ddfb9013bbe8
tamasmagyar <h457040@stud.u-szeged.hu>
2018-12-01 19:51:57 +0100

    products post/get

---
+++ api/controllers/products.js
@@ -0,0 +1,63 @@
+var db = require('./db');
+db.initCollection('products');
+
+module.exports = {
+    createProduct: createProduct,
+    deleteProduct: deleteProduct,
+    getProducts: getProducts,
+    findProduct: findProduct,
+};
+
+function createProduct(req, res) {
+    try {
+        var Product = {
+            name: req.body.name,
+            cost: req.body.cost
+        };
+        var x = db.createObject('products', Product);
+        return res.json(x._id);
+    } catch (error) {
+        return res.send(400, {
+            message: error.message
+        });
+    }
+  }
+  
+function deleteProduct(req, res) {
+    try {
+        var Product = {
+            name: req.body.name,
+            cost: req.body.cost
+        };
+        db.deleteObject('products', product);
+        var x = db.getObjects('products');
+        return res.json(x);
+    } catch (error) {
+        return res.send(400, {
+            message: error.message
+        });
+    }
+  }
+  
+function getProducts(req, res) {
+    try {        
+        var x = db.getObjects('products');
+        return res.json(x);
+    } catch (error) {
+        return res.send(400, {
+            message: error.message
+        });
+    }
+  }
+  
+function findProduct(req, res) {
+    try {        
+        var productName = req.body.name;
+        var x = db.getObjects('products');
+        return res.json(x, productName);
+    } catch (error) {
+        return res.send(400, {
+            message: error.message
+        });
+    }
+  }
