9541fef825c1c83e9e79b5d10a76c1c737a2324f
Rádi Gábor István <h668715@stud.u-szeged,hu>
2018-12-02 22:23:35 +0100

    changed data names

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -15,81 +15,80 @@ function createUser(req, res) {
             username: req.body.username,
             password: req.body.password,
         };
-        if(req.body.balance != null || req.body.balance != ""){
+        if (req.body.balance != null || req.body.balance != "") {
             User.balance = req.body.balance;
-        }else {
+        } else {
             User.balance = 0;
         }
-        var x = db.createObject('users', User);
-        return res.json(x._id);
+        var createdUser = db.createObject('users', User);
+        return res.json(createdUser._id);
     } catch (error) {
-        var x = error.message;
+        var errormessage = error.message;
         const response = {
-            message: x
+            message: errormessage
         }
         return res.status(400).json(response);
     }
-  }
-  
+}
+
 function deleteUser(req, res) {
-    try {        
-        var x = db.deleteObject('users', {username:req.body.username});
-        return res.json(x._id);
+    try {
+        var deletedUser = db.deleteObject('users', { username: req.body.username });
+        return res.json(deletedUser._id);
     } catch (error) {
-        var x = error.message;
+        var errormessage = error.message;
         const response = {
-            message: x
+            message: errormessage
         }
         return res.status(400).json(response);
     }
-  }
-  
+}
+
 function getUser(req, res) {
-    try {        
-        var x = db.getObjects('users');
-        return res.json(x);
+    try {
+        var foundUsers = db.getObjects('users');
+        return res.json(foundUsers);
     } catch (error) {
-        var x = error.message;
+        var errormessage = error.message;
         const response = {
-            message: x
+            message: errormessage
         }
         return res.status(400).json(response);
     }
-  }
-  
+}
+
 function getBalance(req, res) {
-    try {        
-        var x = db.getObject('users', {username:req.swagger.params.username.value});
-        return res.json(x.balance);
+    try {
+        var foundUser = db.getObject('users', { username: req.swagger.params.username.value });
+        return res.json(foundUser.balance);
     } catch (error) {
-        var x = error.message;
+        var errormessage = error.message;
         const response = {
-            message: x
+            message: errormessage
         }
         return res.status(400).json(response);
     }
-  }
-  
+}
+
 function updateBalance(req, res) {
     try {
         var username = req.body.username;
         var amount = req.body.amount;
-        
-        old_user = db.getObject('users', {username:username});
-        
-        var User = {
+
+        old_user = db.getObject('users', { username: username });
+        var newUser = {
             username: old_user.username,
             password: old_user.password,
             balance: old_user.balance + amount
-        };        
-        db.updateObject('users',{username:username}, User)
-        updated_user = db.getObject('users', {username:username});
+        };
+        db.updateObject('users', { username: username }, newUser)
+        updated_user = db.getObject('users', { username: username });
         return res.json(updated_user);
     } catch (error) {
-        var x = error.message;
+        var errormessage = error.message;
         const response = {
-            message: x
+            message: errormessage
         }
         return res.status(400).json(response);
     }
-  }
+}
